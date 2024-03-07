d9a7441e1b51e169a49c953c6a80559feb40ccd2
IsabellaCabrera <158528449+IsabellaCabrera@users.noreply.github.com>
2024-03-07 07:39:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const animal = prompt ("Ingrese un animal");
+if (animal.toLocaleLowerCase () === "perro" ){
+alert( "mi animal es un perro");
+} else if (animal.toLocaleLowerCase ( )=== "gato"){
+    alert ("mi animal es un gato");
+} else if (animal.toLocaleLowerCase () === "conejo") {
+    alert( "mi animal es un conejo");
+} else {
+    alert ("No conozco ese animal")
+}
+const parrafo = document.querySelector("#parrafo");
+console.log(parrafo);
+parrafo.innerHTML = "este es el nuevo contenido del parrafo"
+var varibale
+console.log(variable)
